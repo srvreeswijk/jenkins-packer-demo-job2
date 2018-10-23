99284a0806cb12c3ada4029218520279a61dc2bc
Sebastiaan Vreeswijk <s.vreeswijk@gmail.com>
2018-10-23 22:16:37 +0200

    backend config

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-a2b634h"
-    key    = "jenkins/terraform.tfstate"
+    key    = "jenkins/packer-demo-job2.tfstate"
     region = "eu-central-1"
   }
 }
